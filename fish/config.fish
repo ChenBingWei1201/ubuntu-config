4e1a48e517f71594e00b523d451fa8ac9b94de73
ChenBingWei1201 <rain4030633@gmail.com>
2023-12-16 14:38:07 +0800

    install brew && add thefuck command

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,8 @@ starship init fish | source
 # pnpm
 set -gx PNPM_HOME "/home/joe/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+thefuck --alias | source
