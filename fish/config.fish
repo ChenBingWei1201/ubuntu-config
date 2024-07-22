2c489a80a1799978b043bdecca1dc7adcb1090a4
ChenBingWei1201 <rain4030633@gmail.com>
2024-07-22 14:08:20 +0800

    update: config.fish and kitty.conf

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 starship init fish | source
+
+set -gx PATH $HOME/bin $PATH
 
 # pnpm
 set -gx PNPM_HOME "/home/joe/.local/share/pnpm"
@@ -9,5 +12,12 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+function nvm_auto_use --on-variable PWD
+    if test -f .nvmrc
+        nvm use (cat .nvmrc)
+    end
+end
+
+set -gx PATH /opt/homebrew/bin $PATH
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-thefuck --alias | source
